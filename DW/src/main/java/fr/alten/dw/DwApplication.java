47d1e5abf3b41e5260b16f2f75f0a27398b47c5c
Andy CHABALIER <andy.chabalier@alten.com>
2019-06-20 09:29:18 +0200

    implementing cors configuration

--- DW/src/main/java/fr/alten/dw/DwApplication.java
+++ DW/src/main/java/fr/alten/dw/DwApplication.java
@@ -5,13 +5,19 @@ package fr.alten.dw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DwApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(DwApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DwApplication.class);
 	}
 
 }
---
+++ DW/src/main/java/fr/alten/dw/WebConfiguration.java
@@ -0,0 +1,26 @@
+package fr.alten.dw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 
+ * @author Andy Chabalier
+ *
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Autowired
+	private ApplicationContext ctx;
+
+	@Override
+	public void addCorsMappings(final CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200", ctx.getEnvironment().getProperty("security.allowOrigin"))
+				.allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+	}
+}
