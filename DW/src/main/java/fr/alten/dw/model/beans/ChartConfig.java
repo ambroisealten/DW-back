b29393a1427fd55ebf496d5f696a8659e6da66eb
Andy CHABALIER <andy.chabalier@alten.com>
2019-07-10 09:47:25 +0200

    correction of type for chart Config

--- DW/src/main/java/fr/alten/dw/model/beans/ChartConfig.java
+++ DW/src/main/java/fr/alten/dw/model/beans/ChartConfig.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * 
+ *
  * @author Lucas Royackkers
  *
  */
@@ -18,50 +18,51 @@ import javax.persistence.Table;
 public class ChartConfig implements Serializable{
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 2288365769678297467L;
 	@Id
 	@Column(name="id")
 	private Long id;
 	private String displayed_name;
+	@Column(columnDefinition="LONGTEXT")
 	private String chart_saved;
 	private Date date_of_creation;
 
 	public ChartConfig() {
 		super();
 	}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDisplayed_name() {
-		return displayed_name;
-	}
-
-	public void setDisplayed_name(String displayed_name) {
-		this.displayed_name = displayed_name;
-	}
 
 	public String getChart_saved() {
 		return chart_saved;
-	}
-
-	public void setChart_saved(String chart_saved) {
-		this.chart_saved = chart_saved;
 	}
 
 	public Date getDate_of_creation() {
 		return date_of_creation;
 	}
 
-	public void setDate_of_creation(Date date_of_creation) {
+	public String getDisplayed_name() {
+		return displayed_name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setChart_saved(final String chart_saved) {
+		this.chart_saved = chart_saved;
+	}
+
+	public void setDate_of_creation(final Date date_of_creation) {
 		this.date_of_creation = date_of_creation;
+	}
+
+	public void setDisplayed_name(final String displayed_name) {
+		this.displayed_name = displayed_name;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
 	}
 
 }
